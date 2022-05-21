11f418b132b992aafae0d9dab3f7603a3f5d0cdd
Đỗ Tiến Dũng <dungdotien14@gmail.com>
2022-05-21 18:41:43 +0700

    Fetching data to Movies page

--- src/components/movie/MovieCard.js
+++ src/components/movie/MovieCard.js
@@ -1,7 +1,10 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const MovieCard = ({ item }) => {
-  const { title, vote_average, release_date, poster_path } = item;
+  const { title, vote_average, release_date, poster_path, id } = item;
+  const navigate = useNavigate();
+
   return (
     <div className="movie-card flex flex-col rounded-lg p-3 bg-slate-800 text-white h-full select-none">
       <img
@@ -11,12 +14,15 @@ const MovieCard = ({ item }) => {
       />
       <div className="flex flex-col flex-1">
         <h3 className="text-xl font-bold mb-3">{title}</h3>
-        <div className="flex items-center justify-between text-sm opacity-50 mb-10">
+        <div className="flex items-center justify-between text-sm opacity-50 mb-5">
           <span>{new Date(release_date).getFullYear()}</span>
           <span>{vote_average}</span>
         </div>
       </div>
-      <button className="py-3 px-6 rounded-lg capitalize bg-primary w-full mt-auto">
+      <button
+        onClick={() => navigate(`/movie/${id}`)}
+        className="py-3 px-6 rounded-lg capitalize bg-primary w-full mt-auto"
+      >
         Watch now
       </button>
     </div>
--- src/config.js
+++ src/config.js
@@ -1,1 +1,2 @@
 export const fetcher = (...args) => fetch(...args).then((res) => res.json());
+export const apiKey = "25eac7bfded2875800a2dcebaa8ab051";
